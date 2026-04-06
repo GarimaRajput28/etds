db48aad57e8155461dcedfba733b20acbeb99bda
Aswin Narayanadas <aswin.narayanadas@liferay.com>
2026-04-06 19:10:59 +0530

    Revert files changes

--- tests/tax-e-services.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test } from "../Fixtures/commonSteps"; // your extended test
-import { TaxeService } from "../pages/tax-e-services";
-
-test("external site redirection", async ({ defaultPage }) => {
-  const externalredirectioncheck = new TaxeService(defaultPage);
-  await externalredirectioncheck.externallink();
-});
--- tests/taxinfo-service.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test } from "../Fixtures/commonSteps"; // your extended test
-import { TaxInfoService } from "../pages/taxinfo-services";
-test('@Sanity taxinfo overview redirection check',async({defaultPage})=>{
-    const overviewRedirection = new TaxInfoService(defaultPage);
-    overviewRedirection.taxinfoHeaderRedirection();
-    overviewRedirection.overviewTaxtoolRedirection();
-
-});
